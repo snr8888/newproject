0b5d554085d414cdacebadd21d9103fe0730e91f
snr8888 <raiswapniln@gmail.com>
2021-09-14 02:27:04 +0530

    swapnil committed

---
+++ swapnil.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace CDM.Models
+{
+    public class LocationViewModel
+    {
+        public string SearchText { get; set; }
+        public IEnumerable<LocationModel> LocationList { get; set; }
+        public PagingModel pagingModel { get; set; }
+        public string SortBy { get; set; }
+    }
+}
